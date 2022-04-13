7bae0fe65e3d6d718fd3ca8e7348984692a9e3fe
Saref111 <saref012@gmail.com>
2022-04-13 11:11:45 +0300

    added player controls keys detection

--- js/input.js
+++ js/input.js
@@ -3,19 +3,28 @@ import { Keys } from "./constants.js"
 export default class InputHandler {
     constructor() {
         this.keys = new Set()
+
         window.addEventListener('keydown', this.keydownHandler.bind(this))
         window.addEventListener('keyup', this.keyupHandler.bind(this))
     }
 
     keydownHandler(e) {
-        if (e.key === Keys.LEFT) {
-            this.keys.add(Keys.LEFT)
+        if (this.isControlKeyPressed(e.key)) {
+            this.keys.add(e.key)
         }
+        console.log(this.keys);
     }
     
     keyupHandler(e) {
-        if (e.key === Keys.LEFT) {
-            this.keys.delete(Keys.LEFT)
+        if (this.isControlKeyPressed(e.key)) {
+            this.keys.delete(e.key)
         }
+    }
+
+    isControlKeyPressed(key) {
+        return key === Keys.LEFT || 
+                key === Keys.RIGHT || 
+                key === Keys.UP || 
+                key === Keys.DOWN
     }
 }
