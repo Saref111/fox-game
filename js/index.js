d5765345b8460e1dde3ab535f6dbf8b00fd91c81
Saref111 <saref012@gmail.com>
2022-04-24 18:17:38 +0300

    added loaded state

--- js/index.js
+++ js/index.js
@@ -2,18 +2,25 @@ import Game from './game.js'
 import { CanvasCharsEnum } from './constants.js'
 import { animate } from './helpers.js'
 
+const canvas = document.getElementById('canvas_1')
+const ctx = canvas.getContext('2d')
+const alertContainer = document.querySelector('p.alert')
+
 window.addEventListener('load', () => {
-    const canvas = document.getElementById('canvas_1')
-    const ctx = canvas.getContext('2d')
-    canvas.width = CanvasCharsEnum.WIDTH
-    canvas.height = CanvasCharsEnum.HEIGHT
+    alertContainer.textContent = 'Loaded, tap to start'
+    document.addEventListener('click', () => {
+        alertContainer.remove()
 
-    const game = new Game({
-        width: canvas.width, 
-        height: canvas.height, 
-        ctx,
+        canvas.width = CanvasCharsEnum.WIDTH
+        canvas.height = CanvasCharsEnum.HEIGHT
+
+        const game = new Game({
+            width: canvas.width, 
+            height: canvas.height, 
+            ctx,
+        })
+
+        animate(game)
     })
-
-    animate(game)
     
 })
